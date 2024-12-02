466a362a840284c829632d413aa485ad666c8ee0
Gustavo Rocha <gurocha05@gmail.com>
2024-12-02 20:00:13 -0300

    deletes

--- react_class/organo/src/App.js
+++ react_class/organo/src/App.js
@@ -6,12 +6,6 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-      </header>
     </div>
   );
 }
